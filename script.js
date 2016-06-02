7b728d75de367cf123ccbcb48310dfe60e093586
sonarct <sonarct@gmail.com>
2016-06-02 13:26:17 +0300

    All methods works, some decoration only

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function imageRotator() {
 	//Создаем канвас и добавляем его на странциу.
 	this.canvas = document.createElement('canvas');
 	document.body.appendChild(this.canvas);
-	this.canvas.width = 600;
-	this.canvas.height = 600;
+	this.canvas.width = this.width;
+	this.canvas.height = this.height;
 
 	//Задаем переменную для работы с канвасом в 2д.
 	this.ctx = this.canvas.getContext("2d");
@@ -15,14 +15,13 @@ imageRotator.prototype = {
 	//Подгрузить все картинки в браузер и по onload отдать их в работу.
 	setFrames: function(images) {
 		this.images = images;
-		console.log(images);
 	},
 	
 	//Отрисовать нужный фрэйм по индексу.
 	drawFrame: function(index) {
 		this.index = index;
 		var frame = this.images[index];
-		this.ctx.drawImage(frame,0,0);
+		this.ctx.drawImage(frame,0,0,this.width,this.height);
 	},
 
 	resizeFrame: function(width, height) {
