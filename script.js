a78a0f712f28d16e58c56ac0f289c53e94356c33
sonarct <sonarct@gmail.com>
2016-06-02 11:53:02 +0300

    Some little changes

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 //Главная функция-конструктор. Принимает массив картинок.
 function imageRotator(images) {
 	this.images = images;
+	//Создаем канвас и добавляем его на странциу.
 	var canvas = document.createElement('canvas');
 	document.body.appendChild(canvas);
-
-
 	canvas.width = 600;
 	canvas.height = 600;
+
+	//Задаем переменную для работы с канвасом в 2д.
 	this.ctx = canvas.getContext("2d");
 	
-	this.setFrames(images);
-	this.drawFrame(0);
-		
+
+	// this.setFrames(images);
+	// this.drawFrame(0);	
 };
 
-
-// function drawCanvas(width,height) {
-// 	var canvas = document.createElement('canvas');
-// };
 
 
 
 imageRotator.prototype = {
-	//Подгрузить все картинки в браузер и по onload отдать их в работу
+	//Подгрузить все картинки в браузер и по onload отдать их в работу.
 	setFrames: function(images) {
 		this.images = images;
 		this.frameSet = new Array();
@@ -30,16 +27,18 @@ imageRotator.prototype = {
 			this.frameSet[i] = new Image();
 			this.frameSet[i].src = images[i];
 		};
-		console.log(this.frameSet[5]);
+		console.log(this.frameSet);
 	},
 	
-	//Отрисовать нужный фрэйм по индексу
+	//Отрисовать нужный фрэйм по индексу.
 	drawFrame: function(index) {
 		this.index = index;
 		var ctx = this.ctx;
-		this.frameSet[index].onload = function() {
+		var frame = this.frameSet[index];
+		frame.onload = function() {
 			ctx.drawImage(this,0,0);
-		}	
+		};
+		console.log(this.frameSet[index]);
 	},
 
 };
