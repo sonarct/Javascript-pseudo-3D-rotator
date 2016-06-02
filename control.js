c380f428f0fdadc0386b8f8162e7c801ff3269c3
sonarct <sonarct@gmail.com>
2016-06-02 16:58:15 +0300

    Add mouse drag of picture

--- control.js
+++ control.js
@@ -2,6 +2,8 @@ var images = new Array();
 var myRotator = new imageRotator();
 var imageCount = 72;
 var imagesloaded = 0;
+var number = 0;
+var dragObject = {};
 
 
 for (var i = 0; i < imageCount; i++) {
@@ -19,8 +21,9 @@ for (var i = 0; i < imageCount; i++) {
 
 function allLoaded() {
 	myRotator.setFrames(images);
-	myRotator.drawFrame(44);
+	myRotator.drawFrame(number);
 	getWindowSize();
+	dragFrame();
 };
 
 
@@ -34,4 +37,43 @@ function getWindowSize() {
 	var y = window.innerHeight;
 	var size = Math.min(x,y) * 0.8;
 	myRotator.resizeFrame(size,size);
+};
+
+
+function dragFrame() {
+	var myCanvas = document.getElementById('canvas');
+	myCanvas.ondragstart = function() {
+		return false;
+	};
+	myCanvas.onmousedown = function(e) {
+		dragObject.downX = e.pageX;
+		var counter = 0;		
+		document.onmousemove = function(e) {
+						
+			var moveX = e.pageX - dragObject.downX;
+
+			console.log(number);
+
+			if (counter > moveX) {
+				number++;
+				if (number > imageCount - 1) {
+					number = 0;
+				}
+				myRotator.drawFrame(number);
+			} else if (counter < moveX) {
+				number--;
+				if (number < 0) {
+					number = imageCount - 1;
+				}
+				myRotator.drawFrame(number);
+			};
+
+			counter = moveX;
+		};
+
+		document.onmouseup = function() {
+			document.onmousemove = null;
+			myCanvas.onmouseup = null;
+		};
+	};
 };
