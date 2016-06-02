e76f50cf891dc9e62d65081f68a0296a07906f90
sonarct <sonarct@gmail.com>
2016-06-02 16:08:28 +0300

    Move code form html in file control.js

---
+++ control.js
@@ -0,0 +1,35 @@
+var images = new Array();
+var myRotator = new imageRotator();
+var imageCount = 72;
+var imagesloaded = 0;
+for (var i = 0; i < imageCount; i++) {
+
+	images[i] = new Image();
+	images[i].src = 'img/' + i + '.jpg';
+
+	images[i].onload = function() {
+		imagesloaded++;
+		if (imagesloaded == imageCount) {
+			console.log('ok');
+			allLoaded();
+		}
+	}
+};
+
+function allLoaded() {
+	myRotator.setFrames(images);
+	myRotator.drawFrame(44);
+	getWindowSize();
+};
+
+
+window.onresize = function() {
+	getWindowSize();
+};
+
+function getWindowSize() {
+	var x = window.innerWidth;
+	var y = window.innerHeight;
+	var size = Math.min(x,y) * 0.8;
+	myRotator.resizeFrame(size,size);
+};
