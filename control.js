0123f2ed636d21d32831956712ed8713daf73a37
sonarct <sonarct@gmail.com>
2016-06-07 12:10:08 +0300

    Beautify my code

--- control.js
+++ control.js
@@ -1,29 +1,62 @@
-var images = new Array();
-var myRotator = new imageRotator();
-var imageCount = 72;
-var imagesloaded = 0;
-var number = 0;
-var toggle = 0;
-var canvas = document.getElementById('canvas');
+var images = new Array(),
+	myRotator = new imageRotator(),
+	imageCount = 72,
+	imagesloaded = 0,
+	number = 0,
+	toggle = 0,
+	canvas = document.getElementById('canvas');
 //Coordinates
-var x = 0;
-var tx = 0;
-var lastTx = 0;
-var bx = 0;
+var x = 0,
+	tx = 0,
+	lastTx = 0,
+	bx = 0;
 //Time
-var time = Date.now();
-var diffTime;
-var prevTime;
-var speed = 0;
+var time = Date.now(),
+	diffTime,
+	prevTime,
+	speed = 0;
 //Factors
-var factorRotation = -0.3;
-var factorFriction = 0.97;
-var factorSpeed = 3;
-var factorResize = 0.8;
+var factorRotation = -0.3,
+	factorFriction = 0.97,
+	factorSpeed = 3,
+	factorResize = 0.8;
 
 
 //Initialize work of script
 getImagesArray();
+
+
+//Functions
+function allLoaded() {
+	myRotator.setFrames(images);
+	myRotator.drawFrame(number);
+	getWindowSize();
+	animate();
+};
+
+
+function animate() {
+	prevTime = time;
+	time = Date.now();
+	diffTime = time - prevTime;
+
+	if (!toggle) {
+		tx = tx - factorSpeed * speed;
+		speed = speed * factorFriction;
+		bx = tx;
+	};
+
+	myRotator.drawFrame(frameNumber());
+	requestAnimationFrame(animate);
+};
+
+//Generates index of array according to mouse coordinates
+function frameNumber() {
+	var images = myRotator.images.length
+	number = Math.round(factorRotation * tx);
+	number = ((number % images) + images) % images;
+	return number;
+};
 
 
 function getImagesArray() {
@@ -37,37 +70,6 @@ function getImagesArray() {
 			};
 		};
 	};
-};
-
-
-function allLoaded() {
-	myRotator.setFrames(images);
-	myRotator.drawFrame(number);
-	getWindowSize();
-	animate();
-};
-
-
-function animate() {
-	prevTime = time;
-	time = Date.now();
-	diffTime = time - prevTime;
-	if (!toggle) {
-		tx = tx - factorSpeed * speed;
-		speed = speed * factorFriction;
-		bx = tx;
-	};
-	myRotator.drawFrame(frameNumber());
-	requestAnimationFrame(animate);
-};
-
-
-//Generates index of array according to mouse coordinates
-function frameNumber() {
-	var images = myRotator.images.length
-	number = Math.round(factorRotation * tx);
-	number = ((number % images) + images) % images;
-	return number;
 };
 
 
